2afb3a7850e2f63280c8922a9aef9321a50ad539
Fredi Flores <fredi.florez@jikkosoft.com>
2025-12-18 13:48:03 -0500

    Connet with databes

--- src/infrastructure/lambda/handler.ts
+++ src/infrastructure/lambda/handler.ts
@@ -1,14 +1,17 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'; // Importa los tipos de Lambda
-const PostgresRepository = require('../db/postgresRepository');
-const CountService = require('../../application/countService');
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import PostgresRepository from '../db/postgresRepository';
+import CountService from '../../application/countService';
 
-const repository = new PostgresRepository();
-const service = new CountService(repository);
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const tableName = event.queryStringParameters?.tableName || 'mi_tabla';
 
-exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const tableName = event.queryStringParameters?.tableName || 'mi_tabla'; // Asegúrate de usar queryStringParameters si es de API Gateway
   try {
+    const repository = new PostgresRepository();
+    const service = new CountService(repository);
     const count = await service.execute(tableName);
+
     return {
       statusCode: 200,
       body: JSON.stringify({ count }),
